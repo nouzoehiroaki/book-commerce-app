f550c1122f908a5f3ca81f4225fe88524533b269
nouzoehiroaki <blowitout2007@yahoo.co.jp>
2024-02-03 19:50:03 +0900

    NEXT_PUBLIC_BASE_URL

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export async function POST(request: Request, response: Response){
   const { title, price, bookId, userId} = await request.json();
@@ -25,8 +26,8 @@ export async function POST(request: Request, response: Response){
         }
       ],
       mode: 'payment',
-      success_url: `http://localhost:3000/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `http://localhost:3000`,
+      success_url: `${baseUrl}/book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${baseUrl}`,
     });
     return NextResponse.json({ checkout_url: session.url });
   } catch (err: any){
